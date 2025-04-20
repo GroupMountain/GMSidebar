656105666c4a0a8768248606b605efe227845ce3
n15421 <119112174+n15421@users.noreply.github.com>
2025-04-20 13:11:21 +0800

    Revert "fix: try to fix build"
    This reverts commit d96a127453cd2ef9b281fec149d6e79d77ebca1f.

--- xmake.lua
+++ xmake.lua
@@ -1,4 +1,4 @@
-add_rules("mode.debug", "mode.release", "mode.releasedbg")
+add_rules("mode.debug", "mode.release")
 
 add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
 add_repositories("groupmountain-repo https://github.com/GroupMountain/xmake-repo.git")
@@ -7,19 +7,49 @@ if not has_config("vs_runtime") then
     set_runtimes("MD")
 end
 
-add_requires("levilamina")
+-- Option 1: Use the latest version of LeviLamina released on GitHub.
+add_requires("levilamina", {configs = {target_type = "server"}})
 add_requires("levibuildscript")
 add_requires("gmlib")
 
-target("GMSidebar")
-    add_cxflags( "/EHa", "/utf-8", "/W4", "/w44265", "/w44289", "/w44296", "/w45263", "/w44738", "/w45204")
-    add_defines("NOMINMAX", "UNICODE")
-    add_files("src/**.cpp","src/**.rc")
-    add_includedirs("src")
-    add_packages("levilamina", "gmlib")
+target("GMSidebar") -- Change this to your mod name.
+    add_cxflags(
+        "/EHa",
+        "/utf-8"
+    )
+    add_defines(
+        "NOMINMAX", 
+        "UNICODE", 
+        "_HAS_CXX17",
+        "_HAS_CXX20",
+        "_HAS_CXX23",
+        "GMSidebar_EXPORTS"
+    )
+    add_files(
+        "src/**.cpp",
+        "src/**.rc"
+    )
+    add_includedirs(
+        "src",
+        "include"
+    )
+    add_packages(
+        "levilamina",
+        "gmlib"
+    )
     add_rules("@levibuildscript/linkrule")
-    add_rules("@levibuildscript/modpacker")
-    set_exceptions("none")
+    set_exceptions("none") -- To avoid conflicts with /EHa
     set_kind("shared")
-    set_languages("cxx20")
+    set_languages("cxx23")
     set_symbols("debug")
+
+    after_build(function (target)
+        local mod_packer = import("scripts.after_build")
+
+        local mod_define = {
+            modName = target:name(),
+            modFile = path.filename(target:targetfile()),
+        }
+        
+        mod_packer.pack_mod(target,mod_define)
+    end)
